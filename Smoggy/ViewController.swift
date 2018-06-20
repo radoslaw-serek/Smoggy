ff9db78e4487d67af24c246f65f2aacd24317f62
Radosław Serek <radoslaw.serek@gmail.com>
2018-06-20 20:58:53 +0200

    Initial Commit

---
+++ Smoggy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smoggy
+//
+//  Created by Radosław Serek on 20.06.2018.
+//  Copyright © 2018 Radosław Serek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
